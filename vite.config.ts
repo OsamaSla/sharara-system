7c67e5832bd06de2c03101edbe869bd407c567e6
Osama <osama.sla@gmail.com>
2026-06-07 06:23:18 +0300

    fix: set vite base to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+// vite.config.ts
 export default defineConfig({
   plugins: [react()],
-  base: '/sharara-system/', // כאן הוספת את השורה החשובה
+  base: './', // Change this line
 })
